887d65b4eaac0a36bf761f956de41643e4cf6192
hojkas <ivka.strnadova@gmail.com>
2021-02-11 19:47:51 +0100

    Background and overlay done

--- solver/views.py
+++ solver/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world. This is solver index.")
+    return render(request, 'solver/index.html')
--- sudoku/settings.py
+++ sudoku/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'sudoku'
 ]
 
 MIDDLEWARE = [
@@ -67,7 +68,11 @@ TEMPLATES = [
     },
 ]
 
+STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 WSGI_APPLICATION = 'sudoku.wsgi.application'
 
