d8811b727dc32b6579a50b83c821b9502c2fade3
hojkas <ivka.strnadova@gmail.com>
2021-02-24 22:36:48 +0100

    more work on guides show

--- guides/views.py
+++ guides/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+easy_strategies = {
+    'naked_pair': 'Naked Pair/Triple',
+    'hidden_pair': 'Hidden Pair/Triple'
+}
+medium_strategies = {}
+advanced_strategies = {}
+context = {'easy_strategies': easy_strategies,
+           'medium_strategies': medium_strategies,
+           'advanced_strategies': advanced_strategies}
+
 def index(request):
-    return render(request, 'guides/index.html')
+    return render(request, 'guides/index.html', context)
+
+def detail(request, name):
+    template = 'guides/' + name + '.html'
+    return render(request, template, context)
