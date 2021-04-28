31f00e693cf39aef3a35379c7e3ea8db246964ea
hojkas <ivka.strnadova@gmail.com>
2021-04-29 00:31:25 +0200

    chages for deployment: pt 13 whitenoise and debug false

--- sudoku/settings.py
+++ sudoku/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', 'k+tbu9)0nikn^h(y8qzik8e5!lm77qp8v&*rtzytug$zq*)(md')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
