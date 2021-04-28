c71624d60ecb4bbf2042baf0517d9dd897b44daa
hojkas <ivka.strnadova@gmail.com>
2021-04-28 22:18:44 +0200

    changes for deploy: pt 2

--- sudoku/settings.py
+++ sudoku/settings.py
@@ -74,7 +74,7 @@ TEMPLATES = [
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = "/staticfiles/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
