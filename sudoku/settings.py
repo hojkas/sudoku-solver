e2291f4568c939cc0a1cce3c960dc89166fe6137
hojkas <ivka.strnadova@gmail.com>
2021-04-28 22:19:45 +0200

    changes for deploy: pt 3

--- sudoku/settings.py
+++ sudoku/settings.py
@@ -77,8 +77,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    "sudoku/static"
+    BASE_DIR / "static"
 ]
 
 WSGI_APPLICATION = 'sudoku.wsgi.application'
